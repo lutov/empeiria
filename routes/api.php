32dd9f2a01a01c08631622a89cc00b8240785ddf
lutov <i@vlad-lutov.name>
2024-03-23 14:27:35 +0300

    #1 Update api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,16 @@
 <?php
 
+use App\Http\Controllers\AvatarController;
+use App\Http\Controllers\CharacterController;
+use App\Http\Controllers\ContainerController;
+use App\Http\Controllers\ConversationController;
+use App\Http\Controllers\FactionController;
+use App\Http\Controllers\GenderController;
+use App\Http\Controllers\InventoryController;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\MessageController;
+use App\Http\Controllers\SquadController;
+use App\Http\Controllers\WorldController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,58 +31,67 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     })->name('api.user');
 
     Route::group(array('prefix' => 'factions'), function () {
-        Route::get('/{id}/squads', 'FactionController@squads')->name('faction.squads');
-        Route::post('/{id}/squads/attach', 'FactionController@attachSquads')->name('faction.attach.squads');
-        Route::post('/{id}/squads/detach', 'FactionController@detachSquads')->name('faction.detach.squads');
+        Route::controller(FactionController::class)->group(function () {
+            Route::get('/{id}/squads', 'squads')->name('faction.squads');
+            Route::post('/{id}/squads/attach', 'attachSquads')->name('faction.attach.squads');
+            Route::post('/{id}/squads/detach', 'detachSquads')->name('faction.detach.squads');
 
-        Route::get('/{id}/characters', 'FactionController@characters')->name('faction.characters');
-        Route::post('/{id}/characters/attach', 'FactionController@attachCharacters')->name('faction.attach.characters');
-        Route::post('/{id}/characters/detach', 'FactionController@detachCharacters')->name('faction.detach.characters');
+            Route::get('/{id}/characters', 'characters')->name('faction.characters');
+            Route::post('/{id}/characters/attach', 'attachCharacters')->name('faction.attach.characters');
+            Route::post('/{id}/characters/detach', 'detachCharacters')->name('faction.detach.characters');
+        });
     });
 
     Route::group(array('prefix' => 'squads'), function () {
-        Route::get('/{id}/characters', 'SquadController@characters')->name('squad.characters');
-        Route::post('/{id}/characters/attach', 'SquadController@attachCharacters')->name('squad.attach.characters');
-        Route::post('/{id}/characters/detach', 'SquadController@detachCharacters')->name('squad.detach.characters');
+        Route::controller(SquadController::class)->group(function () {
+            Route::get('/{id}/characters', 'characters')->name('squad.characters');
+            Route::post('/{id}/characters/attach', 'attachCharacters')->name('squad.attach.characters');
+            Route::post('/{id}/characters/detach', 'detachCharacters')->name('squad.detach.characters');
+        });
     });
 
     Route::group(array('prefix' => 'characters'), function () {
-        Route::post('/{id}/move', 'CharacterController@move')->name('move.character');
+        Route::controller(CharacterController::class)->group(function () {
+            Route::post('/{id}/move', 'move')->name('move.character');
+        });
     });
 
     Route::any('avatars/random', 'AvatarController@random')->name('avatar.random');
 
     Route::group(array('prefix' => 'containers'), function () {
-        Route::get('/{id}/items', 'ContainerController@items')->name('container.items');
-        Route::post('/{id}/items/attach', 'ContainerController@attachItems')->name('container.attach.items');
-        Route::post('/{id}/items/detach', 'ContainerController@detachItems')->name('container.detach.items');
+        Route::controller(ContainerController::class)->group(function () {
+            Route::get('/{id}/items', 'items')->name('container.items');
+            Route::post('/{id}/items/attach', 'attachItems')->name('container.attach.items');
+            Route::post('/{id}/items/detach', 'detachItems')->name('container.detach.items');
+        });
     });
 
     Route::group(array('prefix' => 'inventories'), function () {
-        Route::get('/{id}/items', 'InventoryController@items')->name('inventory.items');
-        Route::post('/{id}/items/attach', 'InventoryController@attachItems')->name('inventory.attach.items');
-        Route::post('/{id}/items/detach', 'InventoryController@detachItems')->name('inventory.detach.items');
-        Route::put('/{id}/items/{itemId}', 'InventoryController@setItemValues')->name('inventory.set.item.values');
+        Route::controller(InventoryController::class)->group(function () {
+            Route::get('/{id}/items', 'items')->name('inventory.items');
+            Route::post('/{id}/items/attach', 'attachItems')->name('inventory.attach.items');
+            Route::post('/{id}/items/detach', 'detachItems')->name('inventory.detach.items');
+            Route::put('/{id}/items/{itemId}', 'setItemValues')->name('inventory.set.item.values');
+        });
     });
 
     Route::resources(
         array(
-            'worlds' => 'WorldController',
-            'factions' => 'FactionController',
-            'squads_types' => 'SquadTypeController',
-            'squads' => 'SquadController',
-            'characters' => 'CharacterController',
+            'worlds' => WorldController::class,
+            'factions' => FactionController::class,
+            //'squads_types' => SquadTypeController::class,
+            'squads' => SquadController::class,
+            'characters' => CharacterController::class,
 
-            'items' => 'ItemController',
-            'containers' => 'ContainerController',
-            'inventories' => 'InventoryController',
+            'items' => ItemController::class,
+            'containers' => ContainerController::class,
+            'inventories' => InventoryController::class,
 
-            'avatars' => 'AvatarController',
-            'genders' => 'GenderController',
+            'avatars' => AvatarController::class,
+            'genders' => GenderController::class,
 
-            'conversations' => 'ConversationController',
-            'messages' => 'MessageController',
+            'conversations' => ConversationController::class,
+            'messages' => MessageController::class,
         )
     );
 });
-
