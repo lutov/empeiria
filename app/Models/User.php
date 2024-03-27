ce1d4ebde2d7714a4ab7ad5d48c510a5be4adbce
lutov <i@vlad-lutov.name>
2024-03-27 12:08:08 +0300

    #1 Add an id property to the User Model description to suppress the PHPStorm warning.

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,12 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * Class User
+ * @package App\Models
+ *
+ * @property int $id
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
