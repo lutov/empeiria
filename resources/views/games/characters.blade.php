dea0711cf75c44e6d57fd758bca6fe884aa8140a
lutov <i@vlad-lutov.name>
2024-04-07 20:36:51 +0300

    #3 Add basic params to Character's creation

--- resources/views/games/characters.blade.php
+++ resources/views/games/characters.blade.php
@@ -13,6 +13,43 @@
         $(function() {
             //
         });
+        // TODO refactor
+        function getRandomName() {
+            $.ajax({
+                url: '/api/names/random',
+                type: 'GET',
+                data: {
+                    types: ['first_name']
+                },
+                success: function (result) {
+                    $('#firstName').val(result);
+                }
+            });
+            $.ajax({
+                url: '/api/names/random',
+                type: 'GET',
+                data: {
+                    types: ['nickname']
+                },
+                success: function (result) {
+                    $('#nickname').val(result);
+                }
+            });
+            $.ajax({
+                url: '/api/names/random',
+                type: 'GET',
+                data: {
+                    types: ['last_name']
+                },
+                success: function (result) {
+                    $('#lastName').val(result);
+                }
+            });
+        }
+        function setAge(age)
+        {
+            $('#displayAge').html(age);
+        }
     </script>
 
     <style>
@@ -24,38 +61,75 @@
 
         <div id="newCharacterComponent">
 
-            <div id="carouselExampleIndicators" class="carousel slide w-50">
-                <!--div class="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                </div-->
+            <div class="row">
+                <div class="col">
 
-                <div class="carousel-indicators tmp">
-                    <img src="/img/characters/avatars/newmale/01.png" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1">
-                    <img src="/img/characters/avatars/newmale/02.png" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2">
-                    <img src="/img/characters/avatars/newmale/03.png" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3">
-                </div>
+                    <div class="mb-3">
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="sex" id="inlineRadio1" value="male" checked="checked">
+                            <label class="form-check-label" for="inlineRadio1">Male</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="sex" id="inlineRadio2" value="female">
+                            <label class="form-check-label" for="inlineRadio2">Female</label>
+                        </div>
+                    </div>
 
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="/img/characters/avatars/newmale/01.png" class="d-block w-100" alt="...">
+                    <div class="input-group mb-3">
+                        <input type="text" id="firstName" class="form-control" placeholder="First Name" aria-label="First Name">
+                        <input type="text" id="nickname" class="form-control" placeholder="Nickname" aria-label="Nickname">
+                        <input type="text" id="lastName" class="form-control" placeholder="Last Name" aria-label="Last Name">
+                        <button class="btn btn-outline-secondary" type="button" onclick="getRandomName()">Random</button>
                     </div>
-                    <div class="carousel-item">
-                        <img src="/img/characters/avatars/newmale/02.png" class="d-block w-100" alt="...">
+
+                    <div>
+                        <label for="customRange2" class="form-label">Age: <span id="displayAge">25</span></label>
+                        <input type="range" class="form-range" min="25" max="50" step="5" id="customRange2" onchange="setAge($(this).val())">
                     </div>
-                    <div class="carousel-item">
-                        <img src="/img/characters/avatars/newmale/03.png" class="d-block w-100" alt="...">
-                    </div>
+
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
+                <div class="col">
+
+                    <div id="carouselExampleIndicators" class="carousel slide">
+                        <!--div class="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        </div-->
+
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="/img/characters/avatars/newmale/01.png" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="/img/characters/avatars/newmale/02.png" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="/img/characters/avatars/newmale/03.png" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="/img/characters/avatars/newmale/04.png" class="d-block w-100" alt="...">
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+
+                        <div class="carousel-indicators tmp">
+                            <img src="/img/characters/avatars/newmale/01.png" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1">
+                            <img src="/img/characters/avatars/newmale/02.png" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2">
+                            <img src="/img/characters/avatars/newmale/03.png" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3">
+                            <img src="/img/characters/avatars/newmale/04.png" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4">
+                        </div>
+
+                    </div>
+
+                </div>
             </div>
 
         </div>
