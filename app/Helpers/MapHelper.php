36db117b7295cf22d7eefa341bd10ce266f1c109
lutov <i@vlad-lutov.name>
2024-04-27 00:45:47 +0300

    #2 Working on the MapHelper

--- app/Helpers/MapHelper.php
+++ app/Helpers/MapHelper.php
@@ -4,6 +4,8 @@
 namespace App\Helpers;
 
 
+use Illuminate\Support\Facades\File;
+
 class MapHelper
 {
     public int $worldId;
@@ -14,6 +16,7 @@ class MapHelper
     public array $noiseMap;
     public HeightMapHelper $heightMap;
     public array $biomeMap;
+    public $image;
 
     /**
      * MapHelper constructor.
@@ -45,10 +48,8 @@ class MapHelper
         $seed = $this->seed;
         $octaves = $this->octaves;
         $size = $this->size;
-
         $highestPoint = 0.0;
         $lowestPoint = 0.0;
-
         $map = array();
         $generator = new PerlinNoiseHelper($seed);
         for ($iy = 0; $iy < $size; $iy++) {
@@ -62,10 +63,8 @@ class MapHelper
             $highPoint = max($map[$iy]);
             if($highestPoint < $highPoint) {$highestPoint = $highPoint;}
         }
-
         $this->noiseMap = $map;
         $this->heightMap = new HeightMapHelper($lowestPoint, $highestPoint);
-
         return $map;
     }
 
@@ -78,17 +77,67 @@ class MapHelper
         $tileSize = $this->tileSize;
         $map = $this->noiseMap;
         $heightMap = $this->heightMap;
-
         $biomeMap = array();
         for ($iy = 0; $iy < $size/$tileSize; $iy++) {
             for ($ix = 0; $ix < $size/$tileSize; $ix++) {
                 $biomeMap[$iy][$ix] = BiomeHelper::getIdByTileHeight($map[$iy][$ix], $heightMap);
             }
         }
-
         $this->biomeMap = $biomeMap;
-
         return $biomeMap;
+    }
+
+    /**
+     * @return false|resource
+     */
+    public function getImage()
+    {
+        $size = $this->size;
+        $tileSize = $this->tileSize;
+        $biomeMap = $this->biomeMap;
+        $image = imagecreatetruecolor($size, $size);
+        for ($iy = 0; $iy < $size / $tileSize; $iy++) {
+            for ($ix = 0; $ix < $size / $tileSize; $ix++) {
+                $rgb = BiomeHelper::getRGB($biomeMap[$iy][$ix]);
+                $color = imagecolorallocate($image, $rgb['red'], $rgb['green'], $rgb['blue']);
+                imagefilledrectangle(
+                    $image,
+                    ($ix * $tileSize),
+                    ($iy * $tileSize),
+                    ($ix * $tileSize) + $tileSize,
+                    ($iy * $tileSize) + $tileSize,
+                    $color
+                );
+            }
+        }
+        return $image;
+    }
+
+    /**
+     * @param $image
+     * @param string $filename
+     */
+    public function saveImage($image, string $filename)
+    {
+        $worldId = $this->worldId;
+        $path = 'img/worlds/'.$worldId;
+        File::makeDirectory(public_path($path));
+        $filename = $path.'/'.$filename.'.png';
+        imagepng($image, public_path($filename));
+        imagedestroy($image);
+    }
+
+    /**
+     * @param $image
+     * @return false|string
+     */
+    public function returnImage($image)
+    {
+        ob_start();
+        imagepng($image);
+        $imageContent = ob_get_clean();
+        imagedestroy($image);
+        return $imageContent;
     }
 
 }
