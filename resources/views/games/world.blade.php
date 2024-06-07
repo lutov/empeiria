679800231008095bf3eef2faa4ce9f4212bbefe3
lutov <i@vlad-lutov.name>
2024-06-07 21:16:49 +0300

    #7 Add draggable widget from jquery-ui for the Squad interface

--- resources/views/games/world.blade.php
+++ resources/views/games/world.blade.php
@@ -4,6 +4,19 @@
 
 @section('sidebar')
     @parent
+    <script>
+        $(function() {
+            $( "#sortable" ).sortable({
+                revert: true
+            });
+            $( "#draggable" ).draggable({
+                connectToSortable: "#sortable",
+                helper: "clone",
+                revert: "invalid"
+            });
+            $( "ul, li" ).disableSelection();
+        });
+    </script>
 
     <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
         <div class="offcanvas-header">
@@ -34,7 +47,21 @@
                                             </button>
                                         </h2>
                                         <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
-                                            <div class="accordion-body"></div>
+                                            <div class="accordion-body">
+
+                                                <ul>
+                                                    <li id="draggable" class="ui-state-highlight">Drag me down</li>
+                                                </ul>
+
+                                                <ul id="sortable">
+                                                    <li class="ui-state-default">Item 1</li>
+                                                    <li class="ui-state-default">Item 2</li>
+                                                    <li class="ui-state-default">Item 3</li>
+                                                    <li class="ui-state-default">Item 4</li>
+                                                    <li class="ui-state-default">Item 5</li>
+                                                </ul>
+
+                                            </div>
                                         </div>
                                     </div>
                             @endforeach
