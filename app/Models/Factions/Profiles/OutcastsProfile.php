a0c31a851e994f389db4103e22ae26d3c0b324b9
lutov <i@vlad-lutov.name>
2024-07-17 21:52:50 +0300

    #12 Add Cell de Ray's Caravan Profile

--- app/Models/Factions/Profiles/OutcastsProfile.php
+++ app/Models/Factions/Profiles/OutcastsProfile.php
@@ -6,14 +6,12 @@ namespace App\Models\Factions\Profiles;
 
 use App\Models\Factions\Faction;
 
-class Outcasts
+class OutcastsProfile
 {
-    public int $userId = 0;
     public int $worldId = 0;
 
     public function create()
     {
-        $userId = $this->userId;
         $worldId = $this->worldId;
         $faction = new Faction();
         $faction->world_id = $worldId;
